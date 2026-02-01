c3aa6d8e655b8671c14eb79a58382e7753a1c714
Shiyi Yin <yinshiyi2020@gmail.com>
2026-01-31 21:59:11 -0800

    add mean and median

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use anyhow::Result;
 use kseq::parse_path;
+use medians::Median;
 
 fn main() -> Result<()> {
     let path = std::env::args()
@@ -10,11 +11,13 @@ fn main() -> Result<()> {
     let mut read_count: u64 = 0;
     let mut base_count: u64 = 0;
     let mut gc_count: u64 = 0;
+    let mut lengths: Vec<usize> = Vec::new();
 
     while let Some(record) = records.iter_record().unwrap() {
         read_count += 1;
         let seq = record.seq();
         base_count += seq.len() as u64;
+        lengths.push(seq.len());
 
         gc_count += seq
             .bytes()
@@ -28,8 +31,21 @@ fn main() -> Result<()> {
         0.0
     };
 
+    let mean_len = if read_count > 0 {
+        base_count / read_count
+    } else {
+        0
+    };
+
+    let median_len = lengths
+        .as_mut_slice()
+        .uqmedian(|&x| x as u64)
+        .unwrap();
+
     println!("Total reads : {}", read_count);
     println!("Total bases : {}", base_count);
+    println!("Mean length : {}", mean_len);
+    println!("Median lens : {}", median_len);
     println!("GC%         : {:.2}%", gc_percent);
     Ok(())
 }
